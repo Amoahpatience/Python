52449c039bb74171240665e9683e76da424426a8
amoahpatience <amoahpatience35@gmail.com>
2020-08-13 06:06:10 -0800

    save python work

---
+++ untitled0.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 13 05:19:58 2020
+
+@author: Patience
+"""
+
+anualIncome = 100000.00
+bonus = 420.00
+
+
+year = 12
+
+monthlyIncome = (anualIncome /year) + bonus
+print(monthlyIncome)
